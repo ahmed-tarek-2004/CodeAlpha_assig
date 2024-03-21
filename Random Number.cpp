91ac3b96d9727c4d1f42b0a77b9bc3f011eef09a
Ahmed Tarek <122125653+ahmed-tarek-2004@users.noreply.github.com>
2024-03-21 17:29:12 +0200

    Update Random Number.cpp

--- Random Number.cpp
+++ Random Number.cpp
@@ -4,8 +4,13 @@
 #include<Windows.h>
 #include<ctime>
 using namespace std;
-string state="";
+string state = "";
 int health = 3;
+HANDLE consolehwnd = GetStdHandle(STD_OUTPUT_HANDLE);
+enum colors{
+black = 0, electric = 1,leaf = 2,lightblue = 3,red = 4,darkpurple = 5,gold = 6,lightgrey = 7,grey = 8,blue = 9,green = 10,aqua = 11,lightred = 12,purple = 13,yellow = 14,white = 15,
+};
+
 void underline()
 {
 	for (int i = 0; i < 110; ++i) {
@@ -13,20 +18,24 @@ void underline()
 	}
 	cout << "\n";
 }
+
 void interface_()
 {
 	underline();
+	SetConsoleTextAttribute(consolehwnd, lightblue);
 	cout << "|" << "\t\t\t" << "----------------------------------------------------------------\t\t" << "     |" << endl
 		<< "|" << "\t\t\t" << "|     Welcome To My Game Guess The Random Number   (*^_^*)     |\t\t" << "     |" << endl
-	    <<"|"<<"\t\t\t"<< "----------------------------------------------------------------\t\t" << "     |" << endl;
+		<< "|" << "\t\t\t" << "----------------------------------------------------------------\t\t" << "     |" << endl;
 	underline();
 }
+
 int option()
 {
 	interface_();
 	int choice;
 	do
 	{
+		SetConsoleTextAttribute(consolehwnd, green);
 		underline();
 		cout << "|\t\t\tEnter Your Level Please,  \t\t\t\t\t\t\t     |\n";
 		cout << "|\t\t\t" << "[1]-EASY\t\t\t\t\t\t\t\t\t     |\n"
@@ -34,14 +43,14 @@ int option()
 			<< "|\t\t\t" << "[3]-HARD\t\t\t\t\t\t\t\t\t     |\n"
 			<< "|\t\t\t" << "Enter Your Choice : ";
 		cin >> choice;
-	}
-		while (choice > 3 || choice < 1);
+	} while (choice > 3 || choice < 1);
 	cout << "\t\t\t\t\t\t\t\t\t\t\t|\n";
 	return choice;
 }
+
 void level_interface(int difficult)
 {
-	cout << "|\t\t\t |                   Guessing Number From 1 - "<<difficult<<"\t\t| \t\t     | \n";
+	cout << "|\t\t\t |                   Guessing Number From 1 - " << difficult << "\t\t| \t\t     | \n";
 	cout << "|" << "\t\t\t" << " ----------------------------------------------------------------\t\t" << "     |" << endl;
 	cout << "|" << "\t\t\t" << " ----------------------------------------------------------------\t\t" << "     |" << endl;
 	cout << "|\t\t         |        Trial : " << health << "/3" << "\t\t\t     ";
@@ -57,58 +66,65 @@ void level_interface(int difficult)
 	}
 	cout << "|" << "\t\t\t" << " ----------------------------------------------------------------\t\t" << "     |" << endl;
 }
+
 void level(int difficult)
 {
-	    //level_interface();
-	    health = 3;
-		state = "";
-		srand(time(NULL));
-		int random = rand() % difficult+ 1;
-		int get;
-		do
-		{
-	    level_interface(difficult);
+	//level_interface();
+	health = 3;
+	state = "";
+	srand(time(NULL));
+	int random = rand() % difficult + 1;
+	int get;
+	do
+	{
+		level_interface(difficult);
 		//cout << random<<endl;
-			if (health < 1)
-			{
-				cout << "|                        |           Game Over.. The Number Is "<<random<<"                        |\t\t     |\n";
-				break;
-			}
-			cout << "| Guess The Number Please From 1 - "<<difficult<<" : ";
-			cin >> get;
-			if (get > random)
-			{
-				cout << "|\t\t\t\t  -----------------------\t\t\t\t\t\t     |\n";
-				cout << "|\t\t\t\t\  |  High....Try Again  |\t\t\t\t\t\t     |\n";
-				state = "High";
-				health--;
-				//continue;
-			}
-			else if (get < random)
-			{
-				cout << "|\t\t\t\t  -----------------------\t\t\t\t\t\t     |\n";
-				cout << "|\t\t\t\t  |  Low....Try Again   |\t\t\t\t\t\t     |\n";
-				state = "Low";
-				health--;
-				//continue;
-			}
-			else
-			{
-				cout << "|\t\t\t\t  -----------------------\t\t\t\t\t\t     |\n";
-				cout << "|\t\t\t\t  | Correct... Good Job |\t\t\t\t\t\t     |\n";
-			}
-			underline();
-		    for (int i = 3; i >0; i--)
-		    {
-		    	cout << "Waiting For " << i<<"..s" << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b";
-		    	Sleep(1000);
-		    }
-		    	system("cls");
-				interface_();
+		if (health < 1)
+		{
+			cout << "|                        |           Game Over.. The Number Is " << random << "                        |\t\t     |\n";
+			break;
+		}
+		int color = gold;
+		SetConsoleTextAttribute(consolehwnd, color);
+		cout << "| Guess The Number Please From 1 - " << difficult << " : ";
+		cin >> get;
+		if (get > random)
+		{
+			SetConsoleTextAttribute(consolehwnd, FOREGROUND_RED);
+			cout << "|\t\t\t\t  -----------------------\t\t\t\t\t\t     |\n";
+			cout << "|\t\t\t\t\  |  High....Try Again  |\t\t\t\t\t\t     |\n";
+			state = "High";
+			health--;
+			//continue;
+		}
+		else if (get < random)
+		{
+			SetConsoleTextAttribute(consolehwnd, FOREGROUND_RED);
+			cout << "|\t\t\t\t  -----------------------\t\t\t\t\t\t     |\n";
+			cout << "|\t\t\t\t  |  Low....Try Again   |\t\t\t\t\t\t     |\n";
+			state = "Low";
+			health--;
+			//continue;
+		}
+		else
+		{
+			SetConsoleTextAttribute(consolehwnd, FOREGROUND_GREEN);
+			cout << "|\t\t\t\t  -----------------------\t\t\t\t\t\t     |\n";
+			cout << "|\t\t\t\t  | Correct... Good Job |\t\t\t\t\t\t     |\n";
+		}
+		underline();
+		for (int i = 3; i > 0; i--)
+		{
+			cout << "Waiting For " << i << "..s" << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b";
+			Sleep(1000);
+		}
+		system("cls");
+		interface_();
 
-		} while (get != random);
+	} while (get != random);
 	cout << "|" << "\t\t\t" << " ----------------------------------------------------------------\t\t" << "     |" << endl;
 }
+
 int main()
 {
 	int trial = 1;
